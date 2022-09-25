afdbba84ad3f0b1a8a3347c8c8927cec6bdd53ef
Mike Repec <mikerepec@gmail.com>
2022-09-25 10:59:29 +0200

    Add todo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 import api.RiotApi
 
+// TODO: How best to figure out the best builds available in the current league meta:
+//
+//  1. Hit the /tft/league/v1/challenger endpoint to retrieve a list of Challenger league players
+//  2. Retrieve each player’s PUUID from the the /tft/summoner/v1/summoners/by-name/{summonerName} endpoint
+//  3. Use each player’s PUUID to retrieve latest match stats by:
+//    1. Retrieving the latest match UUIDs from /tft/match/v1/matches/by-puuid/{puuid}/ids
+//    2. Retrieving match data from /tft/match/v1/matches/{matchId}
+//  4. Save this data to a DB to be able to analyse and track it over time
 fun main() {
     val apiKey = System.getenv("RIOT_API_KEY") ?: throw IllegalStateException("Missing RIOT_API_KEY")
     val client = RiotApi(apiKey)
